229813a672779734d0db4197f1389f243c6b8ef1
YiFanCode <15827285578@163.com>
2019-12-31 13:38:04 +0800

    接口环境设置

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,18 @@ module.exports = {
         .set('components', resolve('src/components'))
         .set('pages', resolve('src/pages'))
         .set('api', resolve('src/api'))
+    },
+    devSever: {
+        host: 'localhsot',
+        port: 8080,
+        proxy: {
+            '/api': {
+                target: 'http://mall-pre.springboot.cn',
+                changeOrigin: true,
+                pathRewrite: {
+                    '/api': ''
+                }
+            }
+        }
     }
 }
